69bc2e39c3771de7f249684b65761ef7bf760567
anaskhan28 <khananasatique@gmail.com>
2024-11-12 11:50:00 +0530

    initial commit

---
+++ lottiepro/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center m-4">
+    <h1>LottiePro Landing Page</h1>
+    </div>
+  );
+}
